d1959a0ee9486e40d0822d5d1bfa1b01b115b84e
Anurag Hakeem <anuraghakeem@Anurags-MacBook-Pro.local>
2022-02-26 13:43:27 +0530

    minor fix

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,83 +1,88 @@
-import './style.css'
+import "./style.css";
 
-import * as THREE from 'three';
+import * as THREE from "three";
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-import anurag from './assets/anurag.png'
-import moonImage from './assets/moonImage.jpeg'
-import space from './assets/space.jpeg'
-import normal from './assets/normal.jpeg'
-import backgroundMusic from './assets/backgroundMusic.mp3'
+import anurag from "./assets/anurag.png";
+import moonImage from "./assets/moonImage.jpeg";
+import space from "./assets/space.jpeg";
+import normal from "./assets/normal.jpeg";
+import backgroundMusic from "./assets/backgroundMusic.mp3";
 
 var audio = new Audio(backgroundMusic);
 
 const icon = document.querySelector("#audio-button > i");
 
-const audioButton = document.querySelector('#audio-button')
+const audioButton = document.querySelector("#audio-button");
 
 function playBackgroundMusic() {
-  console.log('clicked');
-  if(audio.paused){
-  audio.play();
-  icon.classList.remove('fa-volume-mute');
-    icon.classList.add('fa-volume-up');
-  }
-  else{
+  console.log("clicked");
+  if (audio.paused) {
+    audio.play();
+    icon.classList.remove("fa-volume-mute");
+    icon.classList.add("fa-volume-up");
+  } else {
     audio.pause();
-    
-    icon.classList.remove('fa-volume-up');
-  icon.classList.add('fa-volume-mute');
+    icon.classList.remove("fa-volume-up");
+    icon.classList.add("fa-volume-mute");
   }
 }
 
-audioButton.onclick = playBackgroundMusic
+audioButton.onclick = playBackgroundMusic;
 
 // playBackgroundMusic()
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector("#bg"),
 });
 
-renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setPixelRatio(window.devicePixelRatio);
 
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(30);
 
-renderer.render( scene, camera );
+renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshStandardMaterial({ color: 0XFF6347 });
-const torus = new THREE.Mesh( geometry, material );
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus)
+scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(20,20,20)     
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight)
+scene.add(pointLight, ambientLight);
 
 // const lightHelper = new THREE.PointLightHelper(pointLight)
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement); 
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial( { color: 0xffffff });
-  const star = new THREE.Mesh( geometry, material ); 
-  const [x, y, z] = Array(3).fill().map(() => THREE. MathUtils.randFloatSpread( 100 ) );
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
   star.position.set(x, y, z);
-  scene.add(star)
+  scene.add(star);
 }
 
-Array(200).fill().forEach(addStar)
+Array(200).fill().forEach(addStar);
 
 const spaceTexture = new THREE.TextureLoader().load(space);
 scene.background = spaceTexture;
@@ -85,8 +90,8 @@ scene.background = spaceTexture;
 // Avatar
 const jeffTexture = new THREE.TextureLoader().load(anurag);
 const jeff = new THREE.Mesh(
- new THREE.BoxGeometry(3,3,3),
- new THREE.MeshBasicMaterial( { map: jeffTexture } )
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: jeffTexture })
 );
 scene.add(jeff);
 
@@ -94,11 +99,11 @@ scene.add(jeff);
 const moonTexture = new THREE.TextureLoader().load(moonImage);
 const normalTexture = new THREE.TextureLoader().load(normal);
 const moon = new THREE.Mesh(
- new THREE.SphereGeometry(3, 32, 32),
- new THREE.MeshStandardMaterial( {
-  map: moonTexture,
-  normalMap: normalTexture
-} )
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
 );
 
 scene.add(moon);
@@ -115,16 +120,16 @@ function moveCamera() {
 
   jeff.rotation.y += 0.01;
   jeff.rotation.z += 0.01;
-  
+
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
   camera.position.y = t * -0.0002;
 }
 
-document.body.onscroll = moveCamera
+document.body.onscroll = moveCamera;
 
 function animate() {
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
@@ -132,7 +137,7 @@ function animate() {
 
   controls.update();
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
-animate()
+animate();
